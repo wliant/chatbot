71c52834f471947aa8d6cab1ece49c81b5edb370
William Tan <tanweilian@outlook.sg>
2019-05-30 19:50:10 +0800

    get graduate programme fee

--- GetGraduateProgrammeFee.py
+++ GetGraduateProgrammeFee.py
@@ -1,8 +1,12 @@
 from pydialogflow_fulfillment import DialogflowResponse
 from pydialogflow_fulfillment import SimpleResponse, Confirmation, OutputContexts, Suggestions
+import json
 
 CONTEXT_ASK_PROGRAMME = "GetGraduateProgrammeFee-ask-programme"
 DATA_FILE = "data/GetGraduateProgrammeFee.json"
+
+with open(DATA_FILE, 'r') as infile:
+    data = json.load(infile)
 
 def has_params(theKey, params):
     return theKey in params and params[theKey] != ""
@@ -16,13 +20,16 @@ def askProgramme(req):
 def process(req):
     params = req.get_paramters()
     print(params)
-    if has_params("graduate-programme", params):
-        return askProgramme(req)
+    #if has_params("graduate-programme", params):
+    #    return askProgramme(req)
     
-    application_group = params["application_group"]
-    graduate_programme = params["graduate-programme"]
+    application_group = "" if "application-group" not in params else params["application-group"] 
+    graduate_programme = "" if "graduate-programme" not in params else params["graduate-programme"]
 
+    result = [item["answer"] for item in data 
+                    if item["graduate-programme"] == graduate_programme 
+                        and item["application-group"] == application_group]
 
-    res = DialogflowResponse("will look up file and return")
-    print(res.get_final_response())
-    return res.get_final_response()
+    if len(result) == 0:
+        return DialogflowResponse("Unknown programme {0}".format(graduate_programme)).get_final_response()
+    return DialogflowResponse("The programme fee is {0}".format(result[0])).get_final_response()
